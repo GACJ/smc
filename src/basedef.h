8ba359ab974cc3ac6490bdfbbe8214af854cb10c
Ted John <ted@brambles.org>
2016-07-29 21:54:12 +0100

    Add filesize function

--- src/basedef.h
+++ src/basedef.h
@@ -7,4 +7,13 @@ enum Bool {FALSE,TRUE};
 
 #define safedelete(p) {delete(p); p=0;}
 
+inline int filesize(const char * path)
+{
+    FILE * fp = fopen(path, "rb");
+    fseek(fp, 0, SEEK_END);
+    long size = ftell(fp);
+    fclose(fp);
+    return (int)size;
+}
+
 #endif // INCBASEDEF
